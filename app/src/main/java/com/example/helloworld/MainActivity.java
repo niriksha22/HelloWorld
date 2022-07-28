a5f9eabdf03647f4e03a86a473c6a61ab6ee5474
niriksha22 <110148651+niriksha22@users.noreply.github.com>
2022-07-28 14:05:06 +0530

    Add files via upload

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -7,15 +7,17 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity", "Hello World debug");
-        Log.e("MainActivity", "Hello World error");
-        Log.w("MainActivity", "Hello World warn");
-        Log.i("MainActivity", "Hello World info");
-        Log.v("MainActivity", "Hello World verbose");
+        Log.d(LOG_TAG, "Debug");
+        Log.e(LOG_TAG, "Error");
+        Log.w(LOG_TAG, "Warn");
+        Log.i(LOG_TAG, "Info");
+        Log.v(LOG_TAG, "Verbose");
     }
 }
